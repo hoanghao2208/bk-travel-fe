b3fc4d0a1fd7930301e01b614c15971ffe7371a8
Dương Hoàng Hảo <duonghoanghao2208@gmail.com>
2024-03-18 14:34:12 +0700

    feat: :sparkles: render information in detail page

--- src/views/DetailTourPage/Inner.jsx
+++ src/views/DetailTourPage/Inner.jsx
@@ -11,21 +11,29 @@ import DetailTourItem from 'components/DetailTourItem';
 import Title from 'components/Title';
 import UserHomePageLayout from 'layouts/UserHomePageLayout';
 import { memo, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './style.scss';
 
-const Inner = memo(() => {
+const Inner = memo(({ tourData }) => {
     useEffect(() => {
         document.title = 'Chi tiết tour';
     });
     const navigate = useNavigate();
+    const { tour_id } = useParams();
+
+    if (!tourData || !tourData.description || !tourData.list_image) {
+        return null;
+    }
+
+    const imageList = JSON.parse(tourData.list_image);
+    const descriptionParagraphs = tourData?.description.split('\r\n\r\n');
+
     return (
         <UserHomePageLayout>
             <div className="tour-detail">
                 <div className="tour-detail__header">
                     <h3 className="tour-detail__header--title">
-                        Đà Nẵng – KDL Bà Nà – Sơn Trà – Hội An – La Vang - Động
-                        Phong Nha – Làng hương Thủy Xuân - Huế
+                        {tourData?.name}
                     </h3>
                     <div className="tour-detail__header--bottom">
                         <p className="tour-detail__header--rate">
@@ -50,100 +58,47 @@ const Inner = memo(() => {
                     <div className="tour-detail__content">
                         <div className="tour-detail__book-tour">
                             <div className="tour-detail__book-tour--image">
-                                <Image src="/images/slide5.jpg" height={500} />
+                                <Image
+                                    src={tourData?.cover_image}
+                                    height={500}
+                                />
                             </div>
                         </div>
                         <div className="tour-detail__infor">
                             <div className="tour-detail__infor--details">
+                                <Title title="Điểm nhấn" />
+                                <p>{tourData?.highlight}</p>
+                            </div>
+                            <div className="tour-detail__infor--details">
                                 <Title title="Chi tiết tour" />
-                                <p>
-                                    Một chuyến đi sẽ vui và thuận tiện biết mấy
-                                    khi không phải lo lắng về phương tiện di
-                                    chuyển trong chuyến đi của mình đúng không
-                                    nào? Dịch vụ đưa đón chung và đưa đón riêng
-                                    đến Safari World - cách Bangkok 45 phút - sẽ
-                                    đưa bạn đến với niềm vui và các cuộc phiêu
-                                    lưu! Dịch vụ đưa đón thoải mái này sẽ không
-                                    chỉ đưa bạn đến và đi từ công viên mà thậm
-                                    chí còn đưa bạn qua công viên Safari Park để
-                                    bạn có thể đến gần các loài động vật. Có
-                                    hàng trăm loại động vật trong công viên,
-                                    không đơn thuần là hươu cao cổ, ngựa vằn, hổ
-                                    và gấu mà còn có các loài chim và bò sát và
-                                    những loài khác. Với phong cách Châu Phi
-                                    hoang dã, công viên sẽ mang lại cho bạn cảm
-                                    giác đang ở Serengeti ngắm nhìn những sinh
-                                    vật tuyệt vời. Bạn sẽ được xem các chương
-                                    trình ngoạn mục tại công viên biển, bao gồm
-                                    chương trình Cowboy Stunt, chương trình biểu
-                                    diễn cá heo và hơn thế nữa! Đừng lo lắng về
-                                    việc quay trở lại Bangkok; xe đưa đón của
-                                    bạn sẽ chờ đón bạn và đưa bạn trở về một
-                                    cách an toàn và thuận tiện.
-                                </p>
-                                <div className="tour-detail__infor--img">
-                                    <Image
-                                        src="/images/slide2.jpg"
-                                        height={400}
-                                    />
-                                    <p className="tour-detail__infor--img-desc">
-                                        Khám phá Dinh Độc Lập
-                                    </p>
-                                </div>
-                                <p>
-                                    Một chuyến đi sẽ vui và thuận tiện biết mấy
-                                    khi không phải lo lắng về phương tiện di
-                                    chuyển trong chuyến đi của mình đúng không
-                                    nào? Dịch vụ đưa đón chung và đưa đón riêng
-                                    đến Safari World - cách Bangkok 45 phút - sẽ
-                                    đưa bạn đến với niềm vui và các cuộc phiêu
-                                    lưu! Dịch vụ đưa đón thoải mái này sẽ không
-                                    chỉ đưa bạn đến và đi từ công viên mà thậm
-                                    chí còn đưa bạn qua công viên Safari Park để
-                                    bạn có thể đến gần các loài động vật. Có
-                                    hàng trăm loại động vật trong công viên,
-                                    không đơn thuần là hươu cao cổ, ngựa vằn, hổ
-                                    và gấu mà còn có các loài chim và bò sát và
-                                    những loài khác. Với phong cách Châu Phi
-                                    hoang dã, công viên sẽ mang lại cho bạn cảm
-                                    giác đang ở Serengeti ngắm nhìn những sinh
-                                    vật tuyệt vời. Bạn sẽ được xem các chương
-                                    trình ngoạn mục tại công viên biển, bao gồm
-                                    chương trình Cowboy Stunt, chương trình biểu
-                                    diễn cá heo và hơn thế nữa! Đừng lo lắng về
-                                    việc quay trở lại Bangkok; xe đưa đón của
-                                    bạn sẽ chờ đón bạn và đưa bạn trở về một
-                                    cách an toàn và thuận tiện.
-                                </p>
-                                <div className="tour-detail__infor--img">
-                                    <Image
-                                        src="/images/slide6.jpg"
-                                        height={400}
-                                    />
-                                    <p className="tour-detail__infor--img-desc">
-                                        Landmark 81 - Tòa nhà cao nhất Việt Nam
-                                    </p>
-                                </div>
+                                {descriptionParagraphs.map(
+                                    (paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                    )
+                                )}
                             </div>
                             <div className="tour-detail__infor--details">
                                 <Title title="Những điều cần lưu ý" />
-                                <p>
-                                    Khi đến nơi, bạn cần điền thông tin vào mẫu
-                                    đơn bảo hiểm. Lưu ý, việc cung cấp thông tin
-                                    cá nhân (VD: Số hộ chiếu, ngày sinh) chỉ
-                                    phục vụ cho mục đích bảo hiểm, đây là yêu
-                                    cầu bắt buộc để đảm bảo an toàn cho bạn
-                                    trong suốt chuyến tham quan. Nếu bạn không
-                                    cung cấp thông tin được yêu cầu, bạn phải ký
-                                    giấy miễn trừ trách nhiệm tại chỗ để tiếp
-                                    tục tham gia chuyến tham quan
-                                </p>
+                                <p>{tourData?.note}</p>
+                            </div>
+                            <div className="tour-detail__infor--details">
+                                <Title title="Một số hình ảnh của tour du lịch" />
+                                <Image.PreviewGroup>
+                                    {imageList.map((image, index) => (
+                                        <div
+                                            className="tour-detail__infor--img"
+                                            key={index}
+                                        >
+                                            <Image src={image} height={400} />
+                                        </div>
+                                    ))}
+                                </Image.PreviewGroup>
                             </div>
                         </div>
                     </div>
                     <div className="tour-detail__feature">
                         <DetailTourItem
-                            title="Từ đ 10.500.802"
+                            title={`Từ ${tourData?.price.toLocaleString()} VNĐ`}
                             icon={<SendOutlined />}
                             buttonTitle="Đặt tour ngay"
                         />
@@ -152,7 +107,9 @@ const Inner = memo(() => {
                             intro="Khám phá lịch trình tour ngay!"
                             icon={<SearchOutlined />}
                             buttonTitle="Xem lịch trình tour"
-                            onClick={() => navigate('/tour/schedule/detail/oo')}
+                            onClick={() =>
+                                navigate(`/tour/schedule/detail/${tour_id}`)
+                            }
                         />
                         <DetailTourItem
                             title="Liên hệ với chúng tôi"
