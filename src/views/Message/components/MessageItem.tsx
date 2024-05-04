b1645f4394e447fbc485fad62bc89e8899882474
Dương Hoàng Hảo <duonghoanghao2208@gmail.com>
2024-05-04 10:50:16 +0700

    feat: :lipstick: add avatar and infor user

--- src/views/Message/components/MessageItem.tsx
+++ src/views/Message/components/MessageItem.tsx
@@ -1,7 +1,9 @@
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
 import { FC, memo } from 'react';
 import { getCustomerId } from 'reducers/token/function';
-import '../styles.scss';
 import { formattedTime } from 'utils/function/format';
+import '../styles.scss';
 
 interface MessageItemProps {
     content: string;
@@ -18,12 +20,22 @@ const MessageItem: FC<MessageItemProps> = memo(({ content, messId, time }) => {
                 messId === userId ? 'display-time' : ''
             }`}
         >
+            <Avatar size={36} icon={<UserOutlined />} />
             <div
-                className={`message-item ${
-                    messId === userId ? 'my-message-item' : ''
+                className={`mess-component ${
+                    messId === userId
+                        ? 'message-component-reverse'
+                        : 'message-component'
                 }`}
             >
-                {content}
+                <span>Khách hàng {messId}</span>
+                <div
+                    className={`message-item ${
+                        messId === userId ? 'my-message-item' : ''
+                    }`}
+                >
+                    {content}
+                </div>
             </div>
             <p className="message-item--time">{formattedTime(time)}</p>
         </div>
