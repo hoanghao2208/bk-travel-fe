a33adde1a99afdf09f518e5ce83b049df0523a5d
Dương Hoàng Hảo <duonghoanghao2208@gmail.com>
2024-05-16 00:19:44 +0700

    feat: update handle message

--- src/views/Message/Inner.jsx
+++ src/views/Message/Inner.jsx
@@ -1,81 +1,53 @@
 import UserHomePageLayout from 'layouts/UserHomePageLayout';
-import { memo, useCallback, useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import messageService from 'services/messageService';
+import { memo } from 'react';
 import NoData from 'views/AdminManageCustomTours/components/NoData';
 import ChatSection from 'views/Message/components/ChatSection';
 import Group from 'views/Message/components/Group';
 import './styles.scss';
-import { getToken } from 'reducers/token/function';
 
-const Inner = memo(({ allGroups, socket }) => {
-    const [searchParams] = useSearchParams();
-    const token = getToken();
-
-    const [activeGrp, setActiveGrp] = useState(
-        Object.fromEntries(searchParams.entries()).selectedGrp || null
-    );
-    const [groupInfo, setGroupInfo] = useState([]);
-
-    const getActiveGroupInfo = useCallback(async () => {
-        try {
-            if (activeGrp) {
-                const response = await messageService.getGroupById(
-                    activeGrp,
-                    token
-                );
-                if (response?.status === 200) {
-                    setGroupInfo(response.data.data);
-                }
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    }, [activeGrp, token]);
-
-    useEffect(() => {
-        getActiveGroupInfo();
-    }, [getActiveGroupInfo]);
-
-    return (
-        <UserHomePageLayout>
-            {allGroups.length === 0 && (
-                <div className="message-nodata">
-                    <NoData />
-                </div>
-            )}
-            {allGroups.length > 0 && (
-                <div className="message">
-                    <div className="message-group">
-                        {allGroups.map(group => (
-                            <Group
-                                name={group.name}
-                                desc={
-                                    group.description === null
-                                        ? 'Nhóm hỗ trợ thông tin đến khách hàng'
-                                        : group.description
-                                }
-                                id={group.group_id}
-                                activeGrp={activeGrp}
-                                setActiveGrp={setActiveGrp}
-                                key={group.group_id}
-                            />
-                        ))}
+const Inner = memo(
+    ({ allGroups, socket, activeGrp, setActiveGrp, groupInfo, allMessage }) => {
+        return (
+            <UserHomePageLayout>
+                {allGroups.length === 0 && (
+                    <div className="message-nodata">
+                        <NoData />
                     </div>
-                    {activeGrp && (
-                        <div className="message-section">
-                            <ChatSection
-                                name={groupInfo?.name}
-                                activeGrp={activeGrp}
-                                socket={socket}
-                            />
+                )}
+                {allGroups.length > 0 && (
+                    <div className="message">
+                        <div className="message-group">
+                            {allGroups.map(group => (
+                                <Group
+                                    name={group.name}
+                                    desc={
+                                        group.description === null
+                                            ? 'Nhóm hỗ trợ thông tin đến khách hàng'
+                                            : group.description
+                                    }
+                                    id={group.group_id}
+                                    activeGrp={activeGrp}
+                                    setActiveGrp={setActiveGrp}
+                                    key={group.group_id}
+                                />
+                            ))}
                         </div>
-                    )}
-                </div>
-            )}
-        </UserHomePageLayout>
-    );
-});
+                        {activeGrp && (
+                            <div className="message-section">
+                                <ChatSection
+                                    name={groupInfo?.name}
+                                    activeGrp={activeGrp}
+                                    socket={socket}
+                                    allMessage={allMessage}
+                                />
+                            </div>
+                        )}
+                    </div>
+                )}
+            </UserHomePageLayout>
+        );
+    }
+);
 
 Inner.displayName = 'Message Inner';
 
